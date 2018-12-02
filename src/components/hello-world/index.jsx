27529ea3d65ccb5be8e7fc541add3f85faf2dcc0
stellalee <stila917@gmail.com>
2018-12-02 21:41:01 +0000

    added styled-components and made Title style component

--- src/components/hello-world/index.jsx
+++ src/components/hello-world/index.jsx
@@ -1,13 +1,26 @@
 import React from 'react';
 import PropTypes from 'prop-types';
 import style from './hello-world.css';
+import styled from 'styled-components';
+
+const Title = styled.h1`
+  font-size: 1.5em;
+  text-align: center;
+  color: palevioletred;
+`;
 
 const HelloWorld = ({ title }) => (
-  <div className={style['hello-world']}>{title}</div>
+  <Title className={style.helloWorld}>{title}</Title>
 );
+
+
+
+
+
+
+
 
 HelloWorld.propTypes = {
   title: PropTypes.string,
 };
-
 export default HelloWorld;
